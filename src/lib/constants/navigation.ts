de145bf336fc02648cd904b646f29604d76ee6da
Toshiki <toshikiii7@outlook.com>
2026-02-18 23:51:22 +0900

    feat(nav): add concept page to secondary navigation

--- src/lib/constants/navigation.ts
+++ src/lib/constants/navigation.ts
@@ -47,6 +47,7 @@ export const PRIMARY_NAV_ITEMS: NavItem[] = [
  */
 export const SECONDARY_NAV_ITEMS: NavItem[] = [
   { href: "/info", labelKey: "nav.info", icon: Info },
+  { href: "/concept", labelKey: "nav.concept", icon: Coffee },
   { href: "/tea-time", labelKey: "nav.teaTime", icon: Coffee },
   { href: "/floor-plan", labelKey: "nav.floorPlan", icon: LayoutGrid },
   { href: "/stats", labelKey: "nav.stats", icon: BarChart3 },
